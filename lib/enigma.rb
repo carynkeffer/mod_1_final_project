b3a2f0385429ce8394292aebb4ca6047c61bcc86
Caryn Keffer <carynkeffer@gmail.com>
2021-01-18 09:10:17 -0500

    encrypt method optional arguments moved out of argument

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,17 @@
 class Enigma
 
+  def encrypt(message, key, date)
+    encrypt_output = Hash.new
+    encrypt_output["encryption:"] = message
+    encrypt_output["key:"] = key || self.random_number_generator
+    encrypt_output["date:"] = date || self.today
+    encrypt_output
+  end
+
+  def decrypt(message, key, date)
+    # create a hash
+  end
+
   def random_number_generator
     key = rand(1000..99999).to_s.rjust(5, "0")
   end
@@ -8,15 +20,4 @@ class Enigma
     Time.now.strftime("%d%m%y")
   end
 
-  def encrypt(message, key = random_number_generator, date = today)
-    encrypt_output = Hash.new
-    encrypt_output["encryption:"] = message
-    encrypt_output["key:"] = key
-    encrypt_output["date:"] = date
-    encrypt_output
-  end
-
-  def decrypt(message, key, date)
-    # create a hash
-  end
 end
