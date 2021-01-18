7e2d0aeca04e8dbdb4dae3d2eceee4305978a28e
Caryn Keffer <carynkeffer@gmail.com>
2021-01-18 17:28:31 -0500

    why does everything break

--- lib/crypt.rb
+++ lib/crypt.rb
@@ -1,5 +1,14 @@
 class Crypt
 
+  enigma = Enigma.new
+
+  file = File.open(ARGV[0], "r")
+  file.read
+  message = gets.chomp
+  file.close
+
+  encrypted = enigma.encrypt(message)
+  
   def encrypted_message(created)
     created
   end
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,5 @@
-require './lib/enigma'
+require './lib/crypt'
 
-enigma = Enigma.new
-
-file = File.open(ARGV[0], "r")
-file.read
-message = gets.chomp
-file.close
-
-encrypted = enigma.encrypt(message)
 
 writer = File.open(ARGV[0], "w")
 writer.write(encrypted)
