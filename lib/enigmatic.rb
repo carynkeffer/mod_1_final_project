edf83d328de81d3c94492b68b29048fb573c2278
Caryn Keffer <carynkeffer@gmail.com>
2021-01-18 18:09:19 -0500

    added new_message(created) method to enigmatic class

--- lib/enigmatic.rb
+++ lib/enigmatic.rb
@@ -6,7 +6,12 @@ class Enigmatic
   def encrypt(message, key, date)
     shifting = Shift.new
     created = shifting.to_letters(message, key, date)
-    created
+    self.new_message(created)
+  end
+
+  def new_message(created)
+    enigma = Enigma.new
+    enigma.encrypted(created)
   end
 
 
