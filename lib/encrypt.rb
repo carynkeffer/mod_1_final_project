bbb5023b724ecce37e255edc2d3bf32282add3b8
Caryn Keffer <carynkeffer@gmail.com>
2021-01-18 18:29:03 -0500

    add open_encrypt method to Crypt class and redirect encrypt.rb to crypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,17 +1,4 @@
-require './lib/enigma'
 require './lib/crypt'
 
-enigma = Enigma.new
-
-file = File.open(ARGV[0], "r")
-file.read
-message = gets.chomp
-file.close
-
-encrypted = enigma.encrypt(message)
-
-writer = File.open(ARGV[0], "w")
-writer.write(encrypted)
-writer.close
-
-puts "Created #{ARGV[0]} with the key #{encrypted["key:"]} and date #{encrypted["date:"]}"
+crypt = Crypt.new
+crypt.open_encrypt
